7a26d39e4cf04c31d628be2319a15de2a0aa509e
ElisaRMA <elisarm.antunes@gmail.com>
2021-02-24 18:41:11 -0300

    changed file name and minor changes on script

--- script climate graph.R
+++ script climate graph.R
@@ -1,23 +1,19 @@
 
-#Climate graph e Heatmap
+#Climate Graph
 
-#data
-
-Year <- c(2014, 2015, 2016)
-Response <- c(1000, 1100, 1200)
-Rate <- c(0.75, 0.42, 0.80)  
 
 library(ggplot2)
 library(readr)
 
-bel <- read.csv("G:/2. LABMETAMASS/COLABORACOES/Bel/bel R.csv")
+#data
+climate <- read.csv("C:/...")
 
-#Chart
-library(ggplot2)
-
+# if you need a specific order on the x axis create an object.
 order <- c("jul/17", "aug/17", "sep/17", "oct/17", "nov/17", "dec/17", "jan/18", "feb/18", "mar/18", "apr/18", "may/18", "jun/18")
  
- ggplot(bel)  + 
+#creates a climate graph with Temperature and Rainfall over the months. (2 axis)
+
+ ggplot(climate)  + 
   geom_bar(aes(x= factor(Month, level = order), y=Rainfall),stat="identity", fill="black", colour="black")+   
   geom_line(aes(x= factor(Month, level = order), y=Average.Temperature),size = 1, color="#252525", group = 1)+
   geom_point(aes(x= factor(Month, level = order), y=Average.Temperature),size = 2, color="#252525", group = 1)+
@@ -28,8 +24,8 @@ order <- c("jul/17", "aug/17", "sep/17", "oct/17", "nov/17", "dec/17", "jan/18",
   scale_y_continuous(sec.axis = sec_axis(~., name = "Temperature (ºC)"))+
   labs( x = "Months", title = "Climate Graph")
 
-  ggsave("bel.png", type = "cairo", dpi = 600 ) #tipo cairo salva o grafico em melhor resoluo
-
+ #better resolution to save the plots
+ggsave("climate.png", type = "cairo", dpi = 600 ) 
 
  
   
